f468ef0c59b3b4c4cb62a846c61c75a76e248487
nao-777 <hmznhobbyak123098@gmail.com>
2023-09-20 21:25:53 +0900

    feat:userRepository ログインIDによりデータの取得、ユーザ情報の登録メソッド

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"threadsAPI/db"
 	"threadsAPI/model"
+	"threadsAPI/repository"
 )
 
 func main() {
+	//テスト用のuser
+	//echoでhttp接続できるようになるまで
+	testUser := model.User{
+		ID:       "test2",
+		LoginID:  "testLogin2",
+		Name:     "tester",
+		Password: "password",
+	}
 	dbConnect := db.OpenPostgresql()
 	dbConnect.AutoMigrate(model.User{})
+	userRepository := repository.NewUserRepository(dbConnect)
+	//サンプルユーザデータの追加
+	userRepository.InsertUser(&testUser)
+	//サンプルユーザデータの取得
+	testUserRes := model.User{}
+	userRepository.GetUserByLoginId(&testUserRes, "testLogin1")
+	fmt.Printf("%+v", testUserRes)
 	defer db.CloseDB(dbConnect)
 }
--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,6 +1,40 @@
 package repository
 
-type IUserRepository interface {
-	//GetUserByID
+import (
+	"threadsAPI/model"
 
+	"gorm.io/gorm"
+)
+
+// ユーザリポジトリのインターフェース
+type IUserRepository interface {
+	GetUserByLoginId(user *model.User, loginId string) error
+	InsertUser(user *model.User) error
+}
+
+// ユーザリポジトリの構造体
+type userRepository struct {
+	db *gorm.DB
+}
+
+// コンストラクタ
+func NewUserRepository(db *gorm.DB) IUserRepository {
+	return &userRepository{db}
+}
+
+// loginIdを使用してユーザ情報を取得する
+func (ur *userRepository) GetUserByLoginId(user *model.User, loginId string) error {
+	//loginIdを基にユーザを特定
+	if err := ur.db.Where("login_id=?", loginId).First(&user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// ユーザ情報の追加
+func (ur *userRepository) InsertUser(user *model.User) error {
+	if err := ur.db.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
 }
