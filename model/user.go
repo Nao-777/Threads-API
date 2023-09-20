65a8cc9321e91ce70555d5e2344a9f09cfbc1c3b
nao-777 <hmznhobbyak123098@gmail.com>
2023-09-20 20:51:57 +0900

    fix:userモデルの修正　loginIDを追加（unique）

--- model/user.go
+++ model/user.go
@@ -5,6 +5,7 @@ import "time"
 //ユーザ構造体
 type User struct {
 	ID        string    `json:"id" gorm:"primaryKey"`
+	LoginID   string    `json:"login_id" gorm:"unique"`
 	Name      string    `json:"name"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
