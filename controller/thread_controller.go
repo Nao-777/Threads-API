1b4047ada4f2185d3517e5d671256f86a68fa715
nao-777 <hmznhobbyak123098@gmail.com>
2023-09-30 19:44:00 +0900

    feat:thread cntrll GetThreadsByUserId

--- controller/thread_controller.go
+++ controller/thread_controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"log"
 	"net/http"
 	"threadsAPI/model"
 	"threadsAPI/usecase"
@@ -10,6 +11,7 @@ import (
 
 type IThreadController interface {
 	CreateThread(c echo.Context) error
+	GetThreadsByUserID(c echo.Context) error
 }
 type threadController struct {
 	tu usecase.IThreadUsecase
@@ -29,4 +31,18 @@ func (tc *threadController) CreateThread(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
 	return nil
+}
+
+// threadデータの取得(ユーザID)
+func (tc *threadController) GetThreadsByUserID(c echo.Context) error {
+	userId := c.Param("id")
+	log.Printf("%s", userId)
+	threads, err := tc.tu.GetThreadsByUserID(userId)
+	if err != nil {
+		return err
+	}
+	for _, v := range threads {
+		log.Printf("%+v\n", v)
+	}
+	return c.JSON(http.StatusOK, threads)
 }
--- router/router.go
+++ router/router.go
@@ -13,5 +13,6 @@ func NewRouter(uc controller.IUserController, tc controller.IThreadController) *
 
 	t := e.Group("/threads")
 	t.POST("", tc.CreateThread)
+	t.GET("/:id", tc.GetThreadsByUserID)
 	return e
 }
