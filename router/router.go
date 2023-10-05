70ad25639474f6ee85979530a10fbddf684d575e
nao-777 <hmznhobbyak123098@gmail.com>
2023-10-05 11:39:31 +0900

    feat:cors(仮)の作成

--- router/router.go
+++ router/router.go
@@ -4,10 +4,17 @@ import (
 	"threadsAPI/controller"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func NewRouter(uc controller.IUserController, tc controller.IThreadController) *echo.Echo {
 	e := echo.New()
+	//corsの設定
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		//アクセスを許可するフロントエンドドメインを追加
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{"GET","POST","PUT","DELETE"},
+	}))
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.Login)
 
