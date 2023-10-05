273086510a1f31aeb038ae8959330532bc33826c
nao-777 <hmznhobbyak123098@gmail.com>
2023-10-05 11:59:39 +0900

    feat: thread res struct

--- model/thread.go
+++ model/thread.go
@@ -11,3 +11,11 @@ type Thread struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdateAt  time.Time `json:"update_at"`
 }
+type ResThread struct{
+	ID string `json:"id"`
+	UserName string `json:"user_name"`
+	LoginID string `json:"login_id"`
+	Title     string    `json:"title"`
+	Contents  string    `json:"contents"`
+	CreatedAt time.Time `json:"created_at"`
+}
